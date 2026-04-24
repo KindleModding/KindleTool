43230a20041edd96e9c5304839f903b92de6c65d
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2026-04-24 14:38:43 +0800

    feat(nix): add nix build system

---
+++ package.nix
@@ -0,0 +1,39 @@
+{
+  stdenv,
+  lib,
+  pkg-config,
+  hostname,
+  libarchive,
+  nettle,
+  zlib,
+}:
+stdenv.mkDerivation {
+  name = "kindletool";
+  src = lib.cleanSource ./.;
+
+  nativeBuildInputs = [
+    pkg-config
+    hostname
+  ];
+
+  buildInputs = [
+    libarchive
+    nettle
+    zlib
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+    make -C KindleTool all
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    mkdir -p $out/share/man/man1
+    install -m 755 KindleTool/Release/kindletool $out/bin/kindletool
+    install -m 644 KindleTool/kindletool.1 $out/share/man/man1/kindletool.1
+    runHook postInstall
+  '';
+}
