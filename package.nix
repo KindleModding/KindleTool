f883524f2eb75cf3806f2bfdca5780ba737c0e12
Sighery <11218602+Sighery@users.noreply.github.com>
2026-04-29 19:15:51 +0200

    Fix hardcoded env error and beautify package.nix
    On my (Linux) NixOS system, the build will just fail because there is no
    `env` under `/usr/bin/` during the build environment.
    
    It could have also been fixed at the Nix derivation level, by replacing
    the hardcoded `/usr/bin/env bash` with `substituteInPlace`, but I
    already changed this hardcoded path once, and bare `env bash` should
    work for all platforms, so I think it's fine to just change it at the
    source.

--- package.nix
+++ package.nix
@@ -7,6 +7,7 @@
   nettle,
   zlib,
 }:
+
 stdenv.mkDerivation {
   name = "kindletool";
   src = lib.cleanSource ./.;
@@ -24,16 +25,27 @@ stdenv.mkDerivation {
 
   buildPhase = ''
     runHook preBuild
+
     make -C KindleTool all
+
     runHook postBuild
   '';
 
   installPhase = ''
     runHook preInstall
+
     mkdir -p $out/bin
     mkdir -p $out/share/man/man1
     install -m 755 KindleTool/Release/kindletool $out/bin/kindletool
     install -m 644 KindleTool/kindletool.1 $out/share/man/man1/kindletool.1
+
     runHook postInstall
   '';
+
+  meta = with lib; {
+    homepage = "https://github.com/NiLuJe/KindleTool";
+    description = "A tool for creating & extracting Kindle updates and more";
+    license = licenses.gpl3;
+    platforms = platforms.all;
+  };
 }
